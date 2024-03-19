73db83d90f02549e28c647e0125ad4340b4ba6d1
Berkansaglamm <berkansaglamm@outlook.com>
2024-03-19 12:14:21 +0300

    Update HackerNews.py

--- HackerNews.py
+++ HackerNews.py
@@ -16,11 +16,10 @@ def news(url):
     for link in links.find_all('a'):
         found_link = link.get('href')
         if found_link:
-            if "https://" in found_link: 
-                if found_links not in found_links:
-                    found_links.append(found_link)
-                    print(i , "- " , found_link)
-                    i += 1
+            if "https://" in found_link and found_links not in found_links: 
+                found_links.append(found_link)
+                print(i , "- " , found_link)
+                i += 1
             if len(found_links) == 30:
                 break
 
